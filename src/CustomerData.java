8513b2aeb1ccbdbf4d6f269af4dae7121a41062c
annalenczowska <lenczowska.a@gmail.com>
2022-11-01 21:31:05 +0100

    Initial commit

---
+++ src/CustomerData.java
@@ -0,0 +1,15 @@
+public class CustomerData {
+        public static void main(String[] args) {
+            String firstName;
+            String lastName;
+            long customerID;
+
+            firstName = "Tomasz";
+            lastName = "Kowalski";
+            customerID = 987654321;
+
+            System.out.println("Welcome, " + firstName + " " + lastName + "!");
+            System.out.println("Your customer ID number is " + customerID + ".");
+
+        }
+}
